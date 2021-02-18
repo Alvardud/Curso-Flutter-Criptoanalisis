03b4a9e9a568aa6c0767c7a298eecec6cd526db2
Alvaro Martinez <alvardud@gmail.com>
2021-02-17 20:11:42 -0400

    tercera clase

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,53 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    //Guia de estilos
     return MaterialApp(
       title: 'Flutter Demo',
+      //Adicion de temas a la aplicacion
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        //Body sera el cuerpo de la aplicacion
+        body: MyHomePage(),
+        //Boton de accion flotante (parte inferior derecha)
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(Icons.edit),
+        ),
+        //Appbar (header o cavecera)
+        appBar: AppBar(
+          actions: [IconButton(onPressed: (){
+            print("icono presionado");
+          },icon: Icon(Icons.menu))],
+          title: Text("Mi primera pantalla"),
+        ),
+      ),
     );
+  }
+}
+
+class MiPrimerWidget extends StatelessWidget {
+
+  String titulo;
+  MiPrimerWidget({this.titulo});
+
+  Widget texto(BuildContext context){
+    return Text(
+                    titulo,style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 30,
+                      decoration: TextDecoration.none
+                  ),
+                );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return texto(context);
   }
 }
 
@@ -80,14 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 height: 100,
                 //child: Center(
-                  child: Text(
-                    "Hola mundo",style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 30,
-                      decoration: TextDecoration.none
-                  //),
-                  ),
-                ),
+                  child: MiPrimerWidget(titulo: "Mi primer widget")
               ),
             ),
             Text(
@@ -97,10 +129,75 @@ class _MyHomePageState extends State<MyHomePage> {
                 decoration: TextDecoration.none
             ),
             ),
+            MiSegundoWidget(),
           ],
         ),
       ),
       color: Colors.green,
+    );
+  }
+}
+
+class MiSegundoWidget extends StatefulWidget {
+  @override
+  _MiSegundoWidgetState createState() => _MiSegundoWidgetState();
+}
+
+class _MiSegundoWidgetState extends State<MiSegundoWidget> {
+
+  ///Stateless Widget => no contienen estados dinamicos (estaticos)
+  ///
+  ///Stateful Widget => contiene estado dinamico y a su vez, este se actualiza mediante
+  /// setState
+  ///y debe ser inicializado mediante
+  /// initState
+  /// 
+  /// dispose
+  /// didChangeDependencies
+  /// 
+  /// Atajos
+  /// stle (Stateless)
+  /// stful (Stateful)
+  /// init (initstate)
+  /// sets (setState) 
+
+  bool encendido;
+
+  @override
+  void initState() {
+    // se inicializa el estado
+    super.initState();
+    encendido = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(height: 100,width: 100,color: encendido?Colors.red:Colors.blue),
+            Container(height: 100,width: 100,color: !encendido?Colors.red:Colors.blue)
+          ],
+        ),
+        GestureDetector(
+          onTap: (){
+            //se actualiza el estado
+            setState(() {
+              encendido = !encendido;
+            });
+          },
+              child: Container(
+            height: 70,
+            width: 300,
+
+            //hacemos referencia al tema mediante el contexto para poder acceder al color primario
+            color: Theme.of(context).primaryColor,
+            child: Center(child: Text("Cambiar",style: TextStyle(decoration: TextDecoration.none))),
+          ),
+        ),
+      ],
     );
   }
 }
